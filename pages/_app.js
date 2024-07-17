2276048e06f25eca5ab7fd3967b0094cbc247dc3
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-07-18 01:55:55 +0300

    build: :heavy_plus_sign: Add sass and primereact styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,13 @@
+// primereact
+import "primereact/resources/themes/lara-light-teal/theme.css";
+// primeflex
+import "primeflex/primeflex.css";
+// primeicons
+import "primeicons/primeicons.css";
+
+// style
+import "../assets/styles/app.scss";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
